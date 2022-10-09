376c2e5495c36f002c3b9c05bba39712dec23989
sittan2 <49196800+sittan2@users.noreply.github.com>
2022-10-10 05:45:44 +0900

    player Move, Breath

--- Assets/Scripts/Sittan/Player.cs
+++ Assets/Scripts/Sittan/Player.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] bool isAlive = false;
+
+
+    Rigidbody Rigidbody;
+    Vector3 movement;
+    [SerializeField] float moveSpeed = 100f;
+
+    [SerializeField] const float maxOxygen = 100f;
+    [SerializeField] float curOxygen = 100f;
+    [SerializeField] float spendOxygen = 10f;
+
+
+
     void Start()
     {
-        
+        Rigidbody = GetComponent<Rigidbody>();
+        curOxygen = maxOxygen;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Breath();
+    }
+
+    void Move(float h, float v)
+    {
+        if (!isAlive) return;
+
+        movement.Set(0, v, h);
+        movement = movement.normalized * moveSpeed * Time.deltaTime;
+
+        Rigidbody.velocity = movement;
+    }
+
+    void Breath()
+    {
+        if (!isAlive) return;
+
+        if (curOxygen > 0)
+        {
+            curOxygen -= spendOxygen * Time.deltaTime;
+        }
+        else
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if (!isAlive) return;
+
+        isAlive = false;
     }
 }
