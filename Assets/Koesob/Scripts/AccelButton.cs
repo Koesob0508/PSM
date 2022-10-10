c0d328cd764ccae2698a943fb9548db947cba783
Koesob <epi3706@khu.ac.kr>
2022-10-10 11:45:51 +0900

    Ship 정상적으로 내부 표시

---
+++ Assets/Koesob/Scripts/AccelButton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelButton : MonoBehaviour
+{
+    public GameObject button;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<Player>();
+
+        if (player != null)
+        {
+            OnButton();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.GetComponent<Player>();
+
+        if (player != null)
+        {
+            OffButton();
+        }
+    }
+
+    private void OnButton()
+    {
+
+    }
+
+    private void OffButton()
+    {
+
+    }
+}
